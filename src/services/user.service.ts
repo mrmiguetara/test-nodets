9a07ab4f00c11355f4a8ff4bc2326263f90e7a0f
mrmiguetara <miguelag1999@hotmail.com>
2021-02-22 18:04:37 -0400

    Added db connection

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,13 +1,15 @@
 import { NextFunction, Request, Response } from "express";
 //importing our model
 import { UserModel } from "../database/users/users.model";
+import { connect, disconnect } from "../database/database";
 import { Document } from 'mongoose';
 
 export class UserService {
   public async getAll(req: Request, res: Response, next: NextFunction) {
     try {
+      await connect();
       const users = await UserModel.find({});
-  
+      await disconnect();
       res.status(200).json({users})
     } catch (error) {
       next(error)
